242f3484f4331d2b7a454f54ded549cd77b9e855
Tikhon Belousko <dazmail01@gmail.com>
2015-11-12 14:53:44 +0200

    Fix task1 and task2 network.

--- ex-12-11/task1.m
+++ ex-12-11/task1.m
@@ -27,29 +27,27 @@ bnet.CPD{D} = tabular_CPD(bnet, D, 'CPT', [0.6 0.4]);
 
 bnet.CPD{I} = tabular_CPD(bnet, I, 'CPT', [0.7 0.3]);
 
-bnet.CPD{G} = tabular_CPD(bnet, G, 'CPT', [0.3      0.4     0.3 ... 
-                                           0.05     0.25    0.7 ...
-                                           0.9      0.08    0.02 ...
-                                           0.5      0.3     0.2]);
+bnet.CPD{G} = tabular_CPD(bnet, G, 'CPT', [0.3      0.05     0.9    0.5 ... 
+                                           0.4      0.25     0.08   0.3 ...
+                                           0.3      0.7      0.02   0.2]);
                                        
-bnet.CPD{S} = tabular_CPD(bnet, S, 'CPT', [0.95     0.05 ...
-                                           0.2      0.8]);
+bnet.CPD{S} = tabular_CPD(bnet, S, 'CPT', [0.95     0.2 ...
+                                           0.05      0.8]);
                                        
-bnet.CPD{L} = tabular_CPD(bnet, L, 'CPT', [0.1      0.9 ...
-                                           0.4      0.6 ...
-                                           0.99     0.01]);
+bnet.CPD{L} = tabular_CPD(bnet, L, 'CPT', [0.1      0.4     0.99 ...
+                                           0.9      0.6     0.01]);
 
 % Inference
 engine = jtree_inf_engine(bnet);
 evidence = cell(1, N);
 % evidence{D} = 1;
-% evidence{I} = 1;
-% evidence{G} = 1;
+%evidence{I} = 1;
+%evidence{G} = 1;
 % evidence{S} = 1;
 % evidence{L} = 1;
 
 [engine, loglik] = enter_evidence(engine, evidence);
-m = marginal_nodes(engine, [D I G S L], [1 1 1 1 1]);
+m = marginal_nodes(engine, [D I G S L]);
 
 m.T
 
